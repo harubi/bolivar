a0f93a5ce9660e2e2c9a799079e6e6f81906d12c
Ahmed Aloufi <936636+harubi@users.noreply.github.com>
2026-03-09 00:08:42 +0700

    test(python): remove redundant dead bridge surface test

--- tests/test_table_stream.py
+++
@@ -1,9 +0,0 @@
-import bolivar._bridge_api as _bridge_api
-
-
-def test_bridge_api_does_not_expose_dead_table_stream():
-    assert not hasattr(_bridge_api, "_extract_tables_stream")
-
-
-def test_bridge_api_does_not_expose_dead_words_stream():
-    assert not hasattr(_bridge_api, "_extract_words_stream")
