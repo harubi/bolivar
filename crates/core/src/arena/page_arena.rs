b2eedeb1747944af3e8d65d0e7c28216d98383af
Ahmed <936636+harubi@users.noreply.github.com>
2026-08-13 15:25:20 +0300

    perf(arena): speed up string interning

--- crates/core/src/arena/page_arena.rs
+++ crates/core/src/arena/page_arena.rs
@@ -1,9 +1,11 @@
-use std::collections::HashMap;
-
 use bumpalo::Bump;
 use lasso::{Rodeo, Spur};
+use rustc_hash::FxHashMap;
+use smol_str::SmolStr;
 
 use super::types::{ArenaChar, ColorId};
+
+type InternCache = FxHashMap<SmolStr, Spur>;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct ColorKey(Box<[u64]>);
@@ -19,15 +21,17 @@ impl ColorKey {
 pub struct PageArena {
     bump: Bump,
     interner: Rodeo,
+    intern_cache: InternCache,
     colors: Vec<Box<[f64]>>,
-    color_index: HashMap<ColorKey, ColorId>,
+    color_index: FxHashMap<ColorKey, ColorId>,
 }
 
 pub struct ArenaContext<'a> {
     bump: &'a Bump,
     interner: &'a mut Rodeo,
+    intern_cache: &'a mut InternCache,
     colors: &'a mut Vec<Box<[f64]>>,
-    color_index: &'a mut HashMap<ColorKey, ColorId>,
+    color_index: &'a mut FxHashMap<ColorKey, ColorId>,
 }
 
 pub trait ArenaLookup {
@@ -44,13 +48,14 @@ impl PageArena {
         Self {
             bump: Bump::new(),
             interner: Rodeo::default(),
+            intern_cache: InternCache::default(),
             colors: Vec::new(),
-            color_index: HashMap::new(),
+            color_index: FxHashMap::default(),
         }
     }
 
     pub fn intern(&mut self, s: &str) -> Spur {
-        self.interner.get_or_intern(s)
+        intern_string(&mut self.interner, &mut self.intern_cache, s)
     }
 
     pub fn resolve(&self, key: Spur) -> &str {
@@ -69,6 +74,7 @@ impl PageArena {
         ArenaContext {
             bump: &self.bump,
             interner: &mut self.interner,
+            intern_cache: &mut self.intern_cache,
             colors: &mut self.colors,
             color_index: &mut self.color_index,
         }
@@ -96,6 +102,7 @@ impl PageArena {
     pub fn reset(&mut self) {
         self.bump.reset();
         self.interner = Rodeo::default();
+        self.intern_cache.clear();
         self.colors.clear();
         self.color_index.clear();
     }
@@ -113,7 +120,7 @@ impl<'a> ArenaContext<'a> {
     }
 
     pub fn intern(&mut self, s: &str) -> Spur {
-        self.interner.get_or_intern(s)
+        intern_string(self.interner, self.intern_cache, s)
     }
 
     pub fn resolve(&self, key: Spur) -> &str {
@@ -134,6 +141,14 @@ impl<'a> ArenaContext<'a> {
     pub fn color(&self, id: ColorId) -> &[f64] {
         &self.colors[id.index()]
     }
+}
+
+fn intern_string(interner: &mut Rodeo, cache: &mut InternCache, value: &str) -> Spur {
+    cache.get(value).copied().unwrap_or_else(|| {
+        let key = interner.get_or_intern(value);
+        cache.insert(SmolStr::new(value), key);
+        key
+    })
 }
 
 impl ArenaLookup for PageArena {
